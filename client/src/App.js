e3e35edc4116c3bfbbd1aafdc36fbc21f649cb19
Allegra <allegra.berndt@gmail.com>
2018-12-28 11:41:55 -0500

    starting to add blueprint

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './resources/triviaLogo.svg';
+import { Button } from '@blueprintjs/core';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
+        <body>
+
+        </body>
       </div>
     );
   }
